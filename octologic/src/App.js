000953800739b9aacdae0b2b75dca3e3c8b4ecdf
shivamj291 <shivamj291@gmail.com>
2023-09-13 09:45:41 +0530

    setup framework and json-server

---
+++ octologic/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+           <h1 className='text-blue-900 bg-red-200 fs-100' >shivam</h1>
+    </div>
+  );
+}
+
+export default App;
