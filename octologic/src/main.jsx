9d227c00f2d9f1e6d0fc15f05f563408a03a1941
shivamj291 <shivamj291@gmail.com>
2023-09-14 00:00:10 +0530

    created view page

--- octologic/src/main.jsx
+++ octologic/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux';
+import Store from './Store/Store.jsx';
+import {BrowserRouter}from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Provider store = {Store}>
+        <App />
+    </Provider>
+   
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
